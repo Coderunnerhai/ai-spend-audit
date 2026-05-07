135abe8904103d0fb6ffa4abbe20b01c3f3f94c2
nikhil-tiwari <nikhiltiwari946@gmail.com>
2026-05-07 16:46:33 +0530

    message

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,23 +1,19 @@
 // frontend/src/App.jsx - Remove HelmetProvider
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AuditPage from './pages/AuditPage';
-import ResultsPage from './pages/ResultsPage';
-import PublicAuditPage from './pages/PublicAuditPage';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import AuditPage from './pages/AuditPage'
+import ResultsPage from './pages/ResultsPage'
+import PublicAuditPage from './pages/PublicAuditPage'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/audit" element={<AuditPage />} />
-        <Route path="/results" element={<ResultsPage />} />
-        <Route path="/audit/:shareableId" element={<PublicAuditPage />} />
-      </Routes>
-    </Router>
-  );
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/audit" element={<AuditPage />} />
+      <Route path="/results" element={<ResultsPage />} />
+      <Route path="/audit/:shareableId" element={<PublicAuditPage />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
