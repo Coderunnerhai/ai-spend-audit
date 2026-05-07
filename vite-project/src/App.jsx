ef33af23a3a83796c6c6047c10cd36d98943373b
nikhil-tiwari <nikhiltiwari946@gmail.com>
2026-05-07 17:29:43 +0530

    message

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,20 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AuditPage from './pages/AuditPage';
-import ResultsPage from './pages/ResultsPage';
-import PublicAuditPage from './pages/PublicAuditPage';
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import AuditPage from './pages/AuditPage'
+import ResultsPage from './pages/ResultsPage'
+import PublicAuditPage from './pages/PublicAuditPage'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/audit" element={<AuditPage />} />
-        <Route path="/results" element={<ResultsPage />} />
-        <Route path="/audit/:shareableId" element={<PublicAuditPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/audit" element={<AuditPage />} />
+      <Route path="/results" element={<ResultsPage />} />
+      <Route path="/audit/:shareableId" element={<PublicAuditPage />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'  // Change this
+import { BrowserRouter } from 'react-router-dom'  // Must have this
 import './index.css'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>  {/* Change from BrowserRouter to HashRouter */}
+    <BrowserRouter>  {/* Must wrap App */}
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
