47635ad3473fc07975799d111cfef004e99c41ae
nikhil-tiwari <nikhiltiwari946@gmail.com>
2026-05-08 13:20:57 +0530

    message

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -11,6 +11,12 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/index.js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
   }
 })
